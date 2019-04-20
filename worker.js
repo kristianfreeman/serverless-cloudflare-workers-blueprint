7fce95a8ec5c229cffd305b613c4a41309dbcf5a
Kristian Freeman <kristian@kristianfreeman.com>
2019-04-20 12:02:07 -0500

    Update worker.js

--- worker.js
+++ worker.js
@@ -1,25 +1,3 @@
 addEventListener("fetch", event => {
-  event.respondWith(handleRequest(event.request))
+  console.log('Hello from workers!')
 })
-
-const constructResponse = (response, body, headers = {}) =>
-  new Response(body, {
-    status: response.status,
-    statusText: response.statusText,
-    headers: Object.assign({}, response.headers, headers),
-  })
-
-const bucketName = "hugo-workers"
-const baseUrl = `http://storage.googleapis.com/${bucketName}`
-
-async function handleRequest(request) {
-  const parsedUrl = new URL(request.url)
-  let path = parsedUrl.pathname
-
-  if (path.endsWith("/")) {
-    path += "index.html"
-  }
-
-  const response = await fetch(`${baseUrl}${path}`)
-  return response
-}
